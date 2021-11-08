65570cd3176377562ebcd7bef80de5980b4b71eb
unknown <jasdeepsaini30@gmail.com>
2021-11-07 20:26:30 -0500

    adding runsearch

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const ctable = require("console.table");
+// const ctable = require("console.table");
 const mysql = require("mysql2");
 const util = require("util");
 
@@ -27,153 +27,170 @@ connection.connect((err) => {
     runSearch();
 });
 
-const runSearch = () => {
+function runSearch() {
     inquirer
         .prompt({
             name: "action",
             type: "list",
-            message: "what would you like to do?",
+            message: "What would you like to do?",
             choices: [
-                "View all employees",
-                "View all employees by department",
-                "View all employees by manager",
-                "Add employee",
-                "Add Department",
-                "Add Role",
-                "Remove employee",
-                "Update employee role",
-                "Update employee manager",
-            ],
-        })
-        .then((answers) => {
+                    "View all employees",
+                    "View all employees by department",
+                    "View all employees by manager",
+                    "Add employee",
+                    "Add Department",
+                    "Add Role",
+                    "Remove employee",
+                    "Update employee role",
+                    "Update employee manager"
+                ]
+                // Promise 
+
+        }).then(answers => {
+            // Start switch statement
             switch (answers.action) {
-                case "view all employees":
+                // Start new case
+                case "View all employees":
+
                     byEmployees();
                     runSearch();
 
                     break;
+                    // Start new case
                 case "View all employees by department":
+
                     byDepartment();
                     runSearch();
 
                     break;
 
                 case "View all employees by manager":
+
                     byManager();
                     runSearch();
 
                     break;
 
+                    // Start new case
+                    // Takes further input
                 case "Add employee":
                     inquirer
                         .prompt([{
                                 name: "employeeFirst",
                                 type: "input",
                                 message: "What is the employee's first name?",
-                                validate: (answer) => {
+                                validate: answer => {
                                     if (answer !== "") {
                                         return true;
                                     }
                                     return "Please enter at least one character.";
-                                },
+                                }
                             },
                             {
                                 name: "employeeLast",
                                 type: "input",
                                 message: "What is the employee's last name?",
-                                validate: (answer) => {
+                                validate: answer => {
                                     if (answer !== "") {
                                         return true;
                                     }
                                     return "Please enter at least one character.";
-                                },
+                                }
                             },
                             {
                                 name: "department",
                                 type: "input",
                                 message: "Please enter the role id",
+
                             },
                             {
                                 name: "manager",
                                 type: "input",
                                 message: "Please enter manager id",
-                            },
-                        ])
-                        .then((answers) => {
-                            addEmployee(
-                                answers.employeeFirst,
-                                answers.employeeLast,
-                                answers.department,
-                                answers.manager
-                            );
+                            }
+                        ]).then(answers => {
+
+                            addEmployee(answers.employeeFirst, answers.employeeLast, answers.department, answers.manager);
                             runSearch();
-                        });
+                        })
 
                     break;
-
+                    // Start new case
+                    // Takes further input
                 case "Add Department":
                     inquirer
                         .prompt([{
-                            name: "Department",
-                            type: "input",
-                            message: "Please enter the department you would like to add?",
-                            validate: (answer) => {
-                                if (answer !== "") {
-                                    return true;
+                                name: "Department",
+                                type: "input",
+                                message: "Please enter the department you would like to add?",
+                                validate: answer => {
+                                    if (answer !== "") {
+                                        return true;
+                                    }
+                                    return "Please enter at least one character.";
                                 }
-                                return "Please enter at least one character.";
                             },
-                        }, ])
-                        .then((answers) => {
+
+                        ]).then(answers => {
+                            // Adds department to database
                             addDepartment(answers.Department);
                             runSearch();
-                        });
+                        })
                     break;
-
+                    // Start new case
+                    // Takes further input
                 case "Add Role":
                     inquirer
                         .prompt([{
                                 name: "title",
                                 type: "input",
                                 message: "Please enter the role's title.",
-                                validate: (answer) => {
+                                validate: answer => {
                                     if (answer !== "") {
                                         return true;
                                     }
                                     return "Please enter at least one character.";
-                                },
+                                }
                             },
                             {
                                 name: "salary",
                                 type: "input",
                                 message: "Please enter the role's salary.",
+
                             },
                             {
                                 name: "department_id",
                                 type: "input",
                                 message: "Please enter the department id.",
-                            },
-                        ])
-                        .then((answers) => {
+
+                            }
+
+                        ]).then(answers => {
+                            // Adds role to database
                             addRole(answers.title, answers.salary, answers.department_id);
                             runSearch();
-                        });
+                        })
                     break;
 
+                    // Start new case
+                    // Takes further input
                 case "Remove employee":
                     inquirer
                         .prompt([{
                             name: "id",
                             type: "input",
                             message: "Please enter the Employee id",
-                        }, ])
-                        .then((answers) => {
+
+                        }]).then(answers => {
+                            // Removes employee to database
                             removeEmployee(answers.id);
                             runSearch();
-                        });
+                        })
                     break;
 
+                    // Start new case
                 case "Update employee role":
+
                     inquirer
                         .prompt([{
                                 name: "employeeId",
@@ -184,16 +201,21 @@ const runSearch = () => {
                                 name: "roleId",
                                 type: "input",
                                 message: "Please enter role's id",
-                            },
-                        ])
-                        .then((answers) => {
+
+                            }
+
+                        ]).then(answers => {
+                            // Updates employee's role
                             updateByRole(answers.employeeId, answers.roleId);
                             runSearch();
-                        });
+
+                        })
 
                     break;
-
+                    // Start new case
+                    // Takes further input
                 case "Update employee manager":
+
                     inquirer
                         .prompt([{
                                 name: "manager",
@@ -204,145 +226,167 @@ const runSearch = () => {
                                 name: "Employee",
                                 type: "input",
                                 message: "Please enter employee id",
-                            },
-                        ])
-                        .then((answers) => {
+
+                            }
+                        ]).then(answers => {
+                            // Updates employee's manager
                             updateByManager(answers.manager, answers.Employee);
                             runSearch();
-                        });
+
+                        })
 
                     break;
             }
-        });
-};
 
-const byEmployees = () => {
-    var results = connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.d_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+        });
+}
+
+// "View all employees",
+function byEmployees() {
+
+    var results = connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.d_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+
 
         function(error, results) {
-            if (error) throw error;
-            console.table(results);
-        }
-    );
+            if (error) throw error
+            console.table(results)
+            runSearch();
+        })
+
 };
 
-const byDepartment = () => {
-    var department = connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, department.d_name FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id;",
+// "View all employees by department",
+function byDepartment() {
+
+    var department = connection.query("SELECT employee.id, employee.first_name, employee.last_name, department.d_name FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id;",
+
 
         function(error, department) {
-            if (error) throw error;
-            console.table(department);
-        }
-    );
+            if (error) throw error
+            console.table(department)
+            runSearch();
+        })
 };
 
-const byManager = () => {
-    var manager = connection.query(
-        "SELECT employee.id, employee.first_name, employee.last_name, department.d_name, employee.manager_id AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id;",
+// "View all employees by manager",
+function byManager() {
+
+    var manager = connection.query("SELECT employee.id, employee.first_name, employee.last_name, department.d_name, employee.manager_id AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id;",
+
 
         function(error, manager) {
-            if (error) throw error;
-            console.table(manager);
-        }
-    );
+            if (error) throw error
+            console.table(manager)
+            runSearch();
+        })
 };
 
-const updateByManager = (managerId, employeeId) => {
+
+// "Update employee manager"
+function updateByManager(managerId, employeeId) {
+
     var updateManager = connection.query(
         "UPDATE employee SET manager_id = ? WHERE id = ?", [managerId, employeeId],
         function(error, updateManager) {
-            if (error) throw error;
-            // console.table(manager)
-        }
-    );
+            if (error) throw error
+                // console.table(manager)
+            runSearch();
+        })
 
     byManager();
-};
 
-const addEmployee = (employeeFirst, employeeLast, department, manager) => {
+}
+
+// "Add employee"
+function addEmployee(employeeFirst, employeeLast, department, manager) {
+
     var add = connection.query(
         "INSERT INTO employee SET first_name = ?, last_name = ?, role_id = ?, manager_id = ?", [employeeFirst, employeeLast, department, manager],
         function(error, add) {
-            if (error) throw error;
-        }
-    );
+            if (error) throw error
+            runSearch();
+        })
 
     byEmployees();
-};
+
+}
 
 // Shows departments only, without employees
-const departmentTable = () => {
-    var depTable = connection.query(
-        "SELECT d_name FROM department;",
+function departmentTable() {
+    var depTable = connection.query("SELECT d_name FROM department;",
+
 
         function(error, depTable) {
-            if (error) throw error;
-            console.table(depTable);
-        }
-    );
-};
+            if (error) throw error
+            console.table(depTable)
+            runSearch();
+        })
+}
 
 // "Add Department"
-const addDepartment = (department) => {
+function addDepartment(department) {
+
     var department = connection.query(
         "INSERT INTO department SET d_name = ?", [department],
         function(error, department) {
-            if (error) throw error;
-            // console.table(manager)
-        }
-    );
+            if (error) throw error
+                // console.table(manager)
+            runSearch();
+        })
 
     departmentTable();
-};
+}
 
-// Shows roles only, without employees:
+// Shows roles only, without employees: 
 
-const roleTable = () => {
-    var roleT = connection.query(
-        "SELECT title, salary, department_id FROM role;",
+function roleTable() {
+    var roleT = connection.query("SELECT title, salary, department_id FROM role;",
 
         function(error, roleT) {
-            if (error) throw error;
-            console.table(roleT);
-        }
-    );
-};
+            if (error) throw error
+            console.table(roleT)
+            runSearch();
+        })
+}
 // "Add role"
-const addRole = (title, salary, department_id) => {
+function addRole(title, salary, department_id) {
+
     var newRole = connection.query(
         "INSERT INTO role SET title = ?, salary = ?, department_id = ?", [title, salary, department_id],
         function(error, newRole) {
-            if (error) throw error;
-            // console.table(manager)
-        }
-    );
+            if (error) throw error
+                // console.table(manager)
+            runSearch();
+        })
 
     roleTable();
-};
+}
+
 
 // "Remove employee"
-const removeEmployee = (id) => {
+function removeEmployee(id) {
+
     var add = connection.query(
         "DELETE FROM employee WHERE id = ?", [id],
         function(error, id) {
-            if (error) throw error;
-        }
-    );
+            if (error) throw error
+            runSearch();
+        })
 
     byEmployees();
-};
+}
 
 // "Update employee role",
-const updateByRole = (employeeId, roleId) => {
+function updateByRole(employeeId, roleId) {
+
     var byRole = connection.query(
         "UPDATE employee SET role_id = ? WHERE id = ?",
 
         [roleId, employeeId],
         function(error, role) {
-            if (error) throw error;
-        }
-    );
+            if (error) throw error
+            runSearch();
+        })
     byDepartment();
-};
+
+}
